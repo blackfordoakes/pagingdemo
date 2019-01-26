35ce43995f1d3be1c9dded965f050897dfa59d78
Stephen Darlington <darlington.stephen@gmail.com>
2019-01-26 11:31:52 -0500

    add Swagger. update Readme.

--- src/Startup.cs
+++ src/Startup.cs
@@ -12,6 +12,7 @@ using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using PagingDemo.Models;
 using PagingDemo.Services;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace PagingDemo
 {
@@ -31,6 +32,12 @@ namespace PagingDemo
 
             services.Configure<PagingOptions>(Configuration.GetSection("DefaultPagingOptions"));
             services.AddSingleton<INumbersServicce, NumbersServicce>();
+
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "Paging Demonstration API", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -44,6 +51,16 @@ namespace PagingDemo
             {
                 app.UseHsts();
             }
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), 
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Paging Demonstration API V1");
+            });
 
             app.UseHttpsRedirection();
             app.UseMvc();
